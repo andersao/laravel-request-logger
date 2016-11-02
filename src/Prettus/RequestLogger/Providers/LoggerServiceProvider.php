7176c3846f7ff942423c682361ffcf93f158a94d
Sazzad Hossain Khan <itsazzad@gmail.com>
2016-11-02 21:00:33 +0600

    removed route before after as not found

--- src/Prettus/RequestLogger/Providers/LoggerServiceProvider.php
+++ src/Prettus/RequestLogger/Providers/LoggerServiceProvider.php
@@ -35,16 +35,12 @@ class LoggerServiceProvider extends ServiceProvider
      */
     public function register()
     {        
-        app('router')->before(function(){
-            Benchmarking::start('application');
-        });
-
-        app('router')->after(function(){
-            Benchmarking::end('application');
-        });
-
+        Benchmarking::start('application');
+        
         $kernel = $this->app->make('Illuminate\Contracts\Http\Kernel');
         $kernel->prependMiddleware(\Prettus\RequestLogger\Middlewares\ResponseLoggerMiddleware::class);
+
+        Benchmarking::end('application');
     }
 
 }
